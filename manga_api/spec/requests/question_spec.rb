78e05e6b0d5a4d9922eb6c9266f9bfdc1d29c062
matsumatsu20 <matsumatsu20@gmail.com>
2015-06-07 18:14:17 +0900

    api test

--- manga_api/spec/requests/question_spec.rb
+++ manga_api/spec/requests/question_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Questions", :type => :request do
 
-  describe "GET /api/v1/questions/naruto" do
+  describe "GET /api/v1/questions/1" do
 
     it "invalid path" do
       # GET /api/v1/questions/naruto にアクセスする
-      get '/api/v1/questions/naruto'
+      get '/api/v1/questions/1'
 
       # ステータスコードの確認
-      expect(response.status).to eq 404
+      expect(response.status).to eq 200
+
+      body = response.body
+      expect(body).to have_json_path('question_array')
     end
   end
 end
