de400a0338cb6050ffeeeec06551a49549fe9d44
haijima <haijima@r.recruit.co.jp>
2015-06-07 09:06:50 +0900

    create mock

---
+++ Manga_android/app/src/main/java/app/android/heidi/kaz/manga/manga/Question.java
@@ -0,0 +1,45 @@
+package app.android.heidi.kaz.manga.manga;
+
+/**
+ * Created by haijimakazuki on 15/06/06.
+ */
+public class Question {
+
+    private String sentence;
+    private String[] choices;
+    private int answer;
+
+    public Question(){
+        super();
+    }
+
+    public Question(String sentence, String[] choices, int answer) {
+        this.sentence = sentence;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public String[] getChoices() {
+        return choices;
+    }
+
+    public void setChoices(String[] choices) {
+        this.choices = choices;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
