12c54625a147e01d5aaa6354da1cc5fa0afd223e
haijima <haijima@r.recruit.co.jp>
2015-06-11 00:03:33 +0900

    (WIP)デザイン適用

--- Manga_android/app/src/main/java/app/android/heidi/kaz/manga/SplashActivity.java
+++ Manga_android/app/src/main/java/app/android/heidi/kaz/manga/SplashActivity.java
@@ -3,7 +3,11 @@ package app.android.heidi.kaz.manga;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -12,13 +16,18 @@ import butterknife.OnClick;
 
 public class SplashActivity extends Activity {
 
-    @InjectView(R.id.start) Button start;
+    @InjectView(R.id.start) ImageButton start;
+    @InjectView(R.id.adView) AdView mAdView;
+    AdRequest adRequest = new AdRequest.Builder().build();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         ButterKnife.inject(this);
+        mAdView.loadAd(adRequest);
     }
 
     @OnClick(R.id.start)
