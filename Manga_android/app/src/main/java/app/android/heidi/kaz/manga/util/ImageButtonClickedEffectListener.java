db97da920fbf41d9543c6476cfa4a520cb70e01d
haijima <haijima@r.recruit.co.jp>
2015-06-13 16:47:19 +0900

    TOPで巻物を押したときに、少し下に移動するエフェクトを追加

--- Manga_android/app/src/main/java/app/android/heidi/kaz/manga/util/ImageButtonClickedEffectListener.java
+++ Manga_android/app/src/main/java/app/android/heidi/kaz/manga/util/ImageButtonClickedEffectListener.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.ImageView;
 
 /**
@@ -14,9 +15,18 @@ public class ImageButtonClickedEffectListener implements OnTouchListener {
     public boolean onTouch(View v, MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             ((ImageView) v).setColorFilter(Color.argb(100, 0, 0, 0));
+            addMargin(v, 0, -10, 0, 0);
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             ((ImageView) v).setColorFilter(null);
+            addMargin(v, 0, 10, 0, 0);
         }
         return false;
     }
+
+    private void addMargin(View v, int left, int top, int right, int bottom){
+        MarginLayoutParams mlp = (MarginLayoutParams)v.getLayoutParams();
+        mlp.setMargins(mlp.leftMargin + left, mlp.topMargin + top, mlp.rightMargin + right, mlp.bottomMargin + bottom);
+        v.setLayoutParams(mlp);
+    }
+
 }
