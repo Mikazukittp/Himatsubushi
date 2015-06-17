8b4eb6aaf3f351f09fe1152a4e05fa9065573b2d
haijima <haijima@r.recruit.co.jp>
2015-06-17 23:17:21 +0900

    botの雛形を作成

---
+++ swbot/bin/index.js
@@ -0,0 +1,33 @@
+var app = require('../app');
+var Twit = require('twit');
+var CronJob = require("cron").CronJob;
+var moment = require('moment');
+//var http = require('http');
+
+var T = new Twit({
+consumer_key: app.get('options').key,
+consumer_secret: app.get('options').secret,
+access_token: app.get('options').token,
+access_token_secret: app.get('options').token_secret
+});
+
+var cronTime = '0 0 0-14 * * *';
+
+new CronJob({
+cronTime: cronTime,
+onTick: function () {
+tweet();
+},
+start: true
+});
+
+function tweet(){
+  //var message = moment().utc().add(9, 'h').format("ただいま MM月DD日 HH時mm分です。");
+  var message = "フォースと共にあれ";
+  console.log(message);
+
+  T.post('statuses/update', { status: message }, function(err, data, response) {
+    //console.log('Tweet!');
+  });
+}
+
